d94b4d95cb3273189f7451fbece81c41bcf76523
linkinkc <linkinkc@gmail.com>
2022-11-17 20:03:22 -0500

    subiendo S16 ejercicios de JS

---
+++ javascript.js
@@ -0,0 +1,58 @@
+//ejercicio 1
+//añadir una función para restar dos números:
+/*
+let a=+prompt("Ingresa el primer numero para restar");
+let b=+prompt("Ingresa el segundo numero para restar");
+
+let multipli=function(x,y){
+    return x*y;
+}
+
+let expon=function(x,y){
+    return Math.pow(x,y);
+}
+
+let suma=(x,y)=>{return x+y};
+
+let resta=(a,b)=> a-b;
+
+
+var result=multipli(5,2);
+document.write(`El resultado de la multiplicación es ${result}<br>`);
+
+result=suma(3,4);
+document.write(`El resultado de la suma es ${result}<br>`);
+
+resultR=resta(a,b);
+document.write(`El resultado de la resta es ${resultR}<br>`);
+*/
+
+//ejercicio 2
+//añadir una función para pasar de Farenheit a Celsius
+/*
+let gradoF=+prompt("Ingresa la temperatura en celsius");
+let gradoC=+prompt("Ingresa la temperatura en farenheit");
+
+let pasaraFarenheit=(gradoF)=> {return fahrenheit = gradoF * 1.8 + 32};
+
+let pasaraCelsius=(gradoC)=> {return celsius = (gradoC-32)/1.8} 
+
+var result = pasaraFarenheit(gradoF);
+document.write(`En la escala de Fahrenheit, le corresponde el número: ${result} <br>`);
+
+var resultC = pasaraCelsius(gradoC);
+document.write(`En la escala de Celsius, le corresponde el número: ${(resultC).toFixed(2)} <br>`);
+*/
+
+//ejercicio 3
+//muestra la suma de dos números introducidos por el usuario.
+/*
+let x=+prompt("Ingresa el primer numero para sumar");
+let y=+prompt("Ingresa el segundo numero para sumar");
+
+let add=(x,y)=>x+y;
+
+
+let resultadoAdd=add(x,y);
+document.write(`La suma de los 2 números es: ${resultadoAdd}.`);
+*/
